472b333df455ab095963b4ec0096c7ad10420af7
Craig Gordon <cyghfer@gmail.com>
2019-12-05 23:41:08 -0500

    Add project + Component file skeleton

---
+++ LiveSplit/Components/LiveSplit.CatchTheRun/CatchTheRunComponent.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Drawing;
+using System.Windows.Forms;
+using LiveSplit.Model;
+using LiveSplit.Options;
+
+namespace LiveSplit.UI.Components
+{
+    public class CatchTheRunComponent : IComponent
+    {
+        public Settings Settings { get; set; }
+
+        protected LiveSplitState State { get; set; }
+        protected Form Form { get; set; }
+        protected TimerModel Model { get; set; }
+
+        public string ComponentName => "Catch The Run";
+
+        public float VerticalHeight => 0;
+        public float MinimumWidth => 0;
+        public float HorizontalWidth => 0;
+        public float MinimumHeight => 0;
+
+        public float PaddingTop => 0;
+        public float PaddingBottom => 0;
+        public float PaddingLeft => 0;
+        public float PaddingRight => 0;
+
+        public IDictionary<string, Action> ContextMenuControls { get; protected set; }
+
+        public CatchTheRunComponent(LiveSplitState state)
+        {
+            Settings = new Settings();
+            Model = new TimerModel();
+            State = state;
+            Form = state.Form;
+            Model.CurrentState = State;
+        }
+
+        public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
+        {
+        }
+
+        public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
+        {
+        }
+        public Control GetSettingsControl(LayoutMode mode)
+        {
+            return Settings;
+        }
+
+        public XmlNode GetSettings(XmlDocument document)
+        {
+            return Settings.GetSettings(document);
+        }
+
+
+        public void SetSettings(XmlNode settings)
+        {
+            Settings.SetSettings(settings);
+        }
+
+        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
