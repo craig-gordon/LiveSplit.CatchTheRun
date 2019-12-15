e2262d7442ff6e6808d16da784fa38f8a1527421
Craig Gordon <cyghfer@gmail.com>
2019-12-15 16:43:14 -0500

    Enable CtR to appear as a Component option in the Layout menu

---
+++ LiveSplit/Components/LiveSplit.CatchTheRun/CatchTheRunComponentFactory.cs
@@ -0,0 +1,27 @@
+﻿using LiveSplit.Model;
+using LiveSplit.UI.Components;
+using System;
+
+[assembly: ComponentFactory(typeof(CatchTheRunComponentFactory))]
+
+namespace LiveSplit.UI.Components
+{
+    public class CatchTheRunComponentFactory : IComponentFactory
+    {
+        public string ComponentName => "Catch The Run";
+
+        public string Description => "Allows stream viewers to receive notifications when speedrunners get on-pace during attempts.";
+
+        public ComponentCategory Category => ComponentCategory.Other;
+
+        public IComponent Create(LiveSplitState state) => new CatchTheRunComponent(state);
+
+        public string UpdateName => ComponentName;
+
+        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.ManualGameTime.xml";
+
+        public string UpdateURL => "http://livesplit.org/update/";
+
+        public Version Version => Version.Parse("0.0.1");
+    }
+}
---
+++ LiveSplit/Components/LiveSplit.CatchTheRun/CatchTheRunSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LiveSplit.UI.Components
+{
+    public partial class Settings : UserControl
+    {
+        public Settings()
+        {
+        }
+
+        public XmlNode GetSettings(XmlDocument document)
+        {
+            var parent = document.CreateElement("Configuration");
+            CreateSettingsNode(document, parent);
+            return parent;
+        }
+
+        private int CreateSettingsNode(XmlDocument document, XmlElement parent)
+        {
+            return SettingsHelper.CreateSetting(document, parent, "Threshold", "test");
+        }
+
+        public void SetSettings(XmlNode settings)
+        {
+        }
+    }
+}
