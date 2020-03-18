9b1ed3b9b97fcd822dadf42fbaaccca9b232291a
Craig Gordon <cyghfer@gmail.com>
2020-03-17 21:03:28 -0400

    Nullify invalid threshold input values

--- LiveSplit/Components/LiveSplit.CatchTheRun/CatchTheRunSettings.cs
+++ LiveSplit/Components/LiveSplit.CatchTheRun/CatchTheRunSettings.cs
@@ -17,7 +17,7 @@ namespace LiveSplit.UI.Components
 {
     public partial class CatchTheRunSettings : UserControl
     {
-        protected IRun Run { get; set; }
+        private IRun Run { get; set; }
 
         internal ClientCredentials Credentials { get; set; }
         internal List<Threshold> Thresholds { get; set; }
@@ -27,7 +27,8 @@ namespace LiveSplit.UI.Components
         internal XmlHelper _XmlHelper { get; set; }
         internal ApiClient _ApiClient { get; set; }
 
-        protected BindingList<Threshold> ThresholdsDataSource { get; set; }
+        private BindingList<Threshold> ThresholdsDataSource { get; set; }
+        private string CurrentlyEditingCellInitialValue { get; set; }
 
         public CatchTheRunSettings(LiveSplitState state)
         {
@@ -44,6 +45,8 @@ namespace LiveSplit.UI.Components
 
             runGrid.DataSource = ThresholdsDataSource;
             runGrid.CellFormatting += runGrid_CellFormatting;
+            runGrid.CellBeginEdit += runGrid_CellBeginEdit;
+            runGrid.CellEndEdit += runGrid_CellEndEdit;
             verifyCredentialsButton.Enabled = !string.IsNullOrWhiteSpace(twitchUsernameTextBox.Text) && !string.IsNullOrWhiteSpace(clientKeyTextBox.Text);
         }
 
@@ -70,6 +73,15 @@ namespace LiveSplit.UI.Components
         {
             verifyCredentialsButton.Enabled = !string.IsNullOrWhiteSpace(twitchUsernameTextBox.Text) && !string.IsNullOrWhiteSpace(clientKeyTextBox.Text);
         }
+        private async void verifyCredentialsButton_Click(object sender, EventArgs e)
+        {
+            await _ApiClient.VerifyClientCredentials(twitchUsernameTextBox.Text, clientKeyTextBox.Text);
+        }
+
+        private void saveCredentialsButton_Click(object sender, EventArgs e)
+        {
+            _XmlHelper.WriteClientCredentials(twitchUsernameTextBox.Text, clientKeyTextBox.Text);
+        }
 
         private void runGrid_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
         {
@@ -77,19 +89,31 @@ namespace LiveSplit.UI.Components
                 e.Value = new ShortTimeFormatter().Format(Run[e.RowIndex].PersonalBestSplitTime[TimingMethod.RealTime]);
         }
 
-        private async void verifyCredentialsButton_Click(object sender, EventArgs e)
+        private void runGrid_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
         {
-            await _ApiClient.VerifyClientCredentials(twitchUsernameTextBox.Text, clientKeyTextBox.Text);
+            if (e.ColumnIndex != Util.THRESHOLD_INDEX)
+                return;
+
+            var cell = runGrid.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            var value = (string)cell.Value;
+            CurrentlyEditingCellInitialValue = value;
+        }
+
+        private void runGrid_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex != Util.THRESHOLD_INDEX)
+                return;
+
+            var cell = runGrid.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            var value = (string)cell.Value;
+
+            if (!Util.IsThresholdInputValid(value))
+                cell.Value = CurrentlyEditingCellInitialValue;
         }
 
         private void saveThresholdsButton_Click(object sender, EventArgs e)
         {
             _XmlHelper.WriteThresholds(Run.FilePath, Util.ConvertDataRowsToDictionary(runGrid.Rows));
-        }
-
-        private void saveCredentialsButton_Click(object sender, EventArgs e)
-        {
-            _XmlHelper.WriteClientCredentials(twitchUsernameTextBox.Text, clientKeyTextBox.Text);
         }
     }
 }
--- LiveSplit/Components/LiveSplit.CatchTheRun/Util.cs
+++ LiveSplit/Components/LiveSplit.CatchTheRun/Util.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LiveSplit.CatchTheRun
@@ -34,6 +31,14 @@ namespace LiveSplit.CatchTheRun
             }
 
             return result;
+        }
+
+        internal static bool IsThresholdInputValid(string input)
+        {
+            if (input[0] == '+' || input[0] == '-')
+                return Double.TryParse(input.Substring(1), out double _);
+            else
+                return false;
         }
     }
 }
